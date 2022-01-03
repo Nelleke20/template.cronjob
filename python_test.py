e3f4231e1f10f6dd144942683d6ae4ddd97e4539
Nelleke Smits <nelleke_s@hotmail.com>
2022-01-03 20:14:40 +0100

    fix(comment): clearify comment

--- python_test.py
+++ python_test.py
@@ -1,5 +1,6 @@
 
 import requests
 
+#create testing telegram message
 resulttext = 'this is a test'
 requests.get("https://api.telegram.org/{}:{}/sendMessage?chat_id={}&text={}".format('bot5012861040', 'AAGXSZHb0lDwpa2hJHpJF1Tv0tK8AVacBjg', '2012057334', resulttext))
